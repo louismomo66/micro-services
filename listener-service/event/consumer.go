4d479618d18fd50358fd2630f2ef97bb9c5117ba
louismomo66 <kwezi.louishg@gmail.com>
2025-05-07 11:28:29 +0300

    rabitMQ

---
+++ listener-service/event/consumer.go
@@ -0,0 +1,22 @@
+package event
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn      *amqp.Connection
+	queueName string
+}
+
+func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+	consumer := Consumer{
+		conn: conn,
+	}
+
+	return consumer, nil
+}
+
+func (consumer *Consumer) Setup() error {
+
+}
